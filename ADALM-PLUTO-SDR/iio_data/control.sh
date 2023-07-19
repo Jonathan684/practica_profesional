0eb5fdfcc11f8839e805d8ba768bf70bd1d3b594
jonathan684 <jonathan.lafalda@gmail.com>
2023-07-19 14:59:43 +0000

    update

--- ADALM-PLUTO-SDR/iio_data/control.sh
+++ ADALM-PLUTO-SDR/iio_data/control.sh
@@ -1,8 +1,9 @@
 #/sys/bus/iio/devices/iio:device0
-ip_address=$(cat ../run/ip.txt)
+ip_address=$(cat ../../test/ip.txt)
 echo "=========================================="
 sshpass -p "analog" ssh -t root@"$ip_address" "cd /sys/bus/iio/devices/iio:device0; 
 echo -n "out_altvoltage1_TX_LO_frequency-:" ; cat out_altvoltage1_TX_LO_frequency
+echo -n "out_altvoltage1_TX_LO_frequency_available-:" ;  cat out_altvoltage1_TX_LO_frequency_available
 echo -n "out_altvoltage0_RX_LO_frequency-:" ; cat out_altvoltage0_RX_LO_frequency
 echo -n "out_voltage_rf_bandwidth--------:" ; cat out_voltage_rf_bandwidth
 echo -n "in_voltage_rf_bandwidth---------:" ; cat in_voltage_rf_bandwidth
@@ -12,6 +13,8 @@ echo -n "filter_fir_config---------------:" ; cat filter_fir_config
 echo -n "tx_path_rates-------------------:" ; cat tx_path_rates
 echo -n "in_voltage0_hardwaregain rx-----:" ; cat in_voltage0_hardwaregain
 echo -n "out_voltage0_hardwaregain tx----:" ; cat out_voltage0_hardwaregain
+echo -n "in_voltage0_hardwaregain_available rx----:" ; cat in_voltage0_hardwaregain_available
+echo -n "out_voltage0_hardwaregain_available tx----:" ; cat out_voltage0_hardwaregain_available
 echo -n "in_voltage0_gain_control_mode --:" ; cat in_voltage0_gain_control_mode
 echo -n "out_voltage_sampling_frequency_available --:" ; cat out_voltage_sampling_frequency_available
 echo -n "in_voltage_sampling_frequency_available --:" ; cat in_voltage_sampling_frequency_available"
--- ADALM-PLUTO-SDR/iio_data/look_for_info.sh
+++ ADALM-PLUTO-SDR/iio_data/look_for_info.sh
@@ -3,7 +3,7 @@
 #Buscar el archivo.
 #touch salida.txt
 #iio_info >> salida.txt
-ip_address=$(cat ../run/ip.txt)
+ip_address=$(cat ../../test/ip.txt)
 sshpass -p "analog" ssh -t root@"$ip_address" "cd /proyecto_radar/;rm iio_info.txt; touch iio_info.txt ; iio_info >> iio_info.txt"
 echo -n > iio_info.txt
 sshpass -p "analog" scp  root@"$ip_address":/proyecto_radar/iio_info.txt ../iio_data
